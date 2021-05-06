55f480706eb1fe1e9ddadad172b5074d873feaaf
marcio72vieira <marcio72vieira@hotmail.com>
2021-05-06 16:29:46 -0300

    Uppercase do nome do Associado no arquivo CSV

--- resources/views/admin/associado/listaassociadoscsstable.blade.php
+++ resources/views/admin/associado/listaassociadoscsstable.blade.php
@@ -9,10 +9,13 @@
     </thead>
     <tbody>
         @foreach($associados as $associado)
-             @php $pathimagem = str_replace("fotos/", "", $associado->imagem); @endphp
+             @php
+                $pathimagem = str_replace("fotos/", "", $associado->imagem);
+                $uppernome = mb_strtoupper($associado->nome);
+            @endphp
              <tr>
                 <td>{{$associado->id}}</td>
-                <td>{{$associado->nome}}</td>
+                <td>{{$uppernome}}</td>
                 <td>{{$associado->cpf}}</td>
                 <td>{{$pathimagem}}</td>
              </tr>
