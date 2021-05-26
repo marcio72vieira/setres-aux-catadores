12a4006bb77bb4f8e5019aaac994d6a8dbe7b2d1
marcio72vieira <marcio72vieira@hotmail.com>
2021-05-26 11:10:31 -0300

    Adequação dos Dados de Retorno do Associado para a API

--- app/Http/Controllers/Api/AssociadoController.php
+++ app/Http/Controllers/Api/AssociadoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Associado;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class AssociadoController extends Controller
@@ -17,8 +18,43 @@ class AssociadoController extends Controller
     public function index()
     {
         // postman GET = http://localhost:8000/api/associados
+        // $associados = Associado::all();
 
-        $associados = Associado::all();
+        $associados = DB::table('associados')
+        ->join('companhias', 'companhias.id', '=', 'associados.companhia_id')
+        ->join('municipios', 'municipios.id', '=', 'associados.municipio_id')
+        ->join('bairros', 'bairros.id', '=', 'associados.bairro_id')
+        ->select(
+            'associados.id',
+            'associados.nome',
+            'associados.nascimento',
+            'associados.rg',
+            'associados.rgorgaoemissor',
+            'associados.cpf',
+            'associados.sexo',
+            'associados.racacor',
+            'associados.filiacao',
+            'associados.quantidade',
+            'associados.endereco',
+            'associados.numero',
+            'associados.bairro_id',
+            'bairros.nome AS nomebairro',
+            'associados.complemento',
+            'associados.municipio_id',
+            'municipios.nome AS nomemunicipio',
+            'associados.zona',
+            'associados.foneum',
+            'associados.fonedois',
+            'associados.imagem',
+            'associados.idqrcode',
+            'associados.imagemqrcode',
+            'associados.companhia_id',
+            'companhias.nome AS nomecompanhia',
+            'associados.created_at',
+            'associados.updated_at',)
+        ->orderBy('associados.id', 'ASC')
+        ->get();
+
         return response()->json($associados);
     }
 
@@ -43,8 +79,43 @@ class AssociadoController extends Controller
     {
 
         // postman GET = http://localhost:8000/api/associado/1
+        //$associado = Associado::where('id', '=', $id)->get();
 
-        $associado = Associado::where('id', '=', $id)->get();
+        $associado = DB::table('associados')
+        ->join('companhias', 'companhias.id', '=', 'associados.companhia_id')
+        ->join('municipios', 'municipios.id', '=', 'associados.municipio_id')
+        ->join('bairros', 'bairros.id', '=', 'associados.bairro_id')
+        ->select(
+            'associados.id',
+            'associados.nome',
+            'associados.nascimento',
+            'associados.rg',
+            'associados.rgorgaoemissor',
+            'associados.cpf',
+            'associados.sexo',
+            'associados.racacor',
+            'associados.filiacao',
+            'associados.quantidade',
+            'associados.endereco',
+            'associados.numero',
+            'associados.bairro_id',
+            'bairros.nome AS nomebairro',
+            'associados.complemento',
+            'associados.municipio_id',
+            'municipios.nome AS nomemunicipio',
+            'associados.zona',
+            'associados.foneum',
+            'associados.fonedois',
+            'associados.imagem',
+            'associados.idqrcode',
+            'associados.imagemqrcode',
+            'associados.companhia_id',
+            'companhias.nome AS nomecompanhia',
+            'associados.created_at',
+            'associados.updated_at',)
+        ->where('associados.id', '=', $id )
+        ->get();
+
         return response()->json($associado);
     }
 
